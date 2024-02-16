a712c36e964a516c7e3d1847839fb8cd46be261a
Sidra Gondal <150411149+geekycoder2@users.noreply.github.com>
2024-02-17 00:41:00 +0500

    Add files via upload

---
+++ Q12.js
@@ -0,0 +1,8 @@
+"use strict";
+// 12.Greetings: Start with the array you used in Exercise 11, but instead of just printing each person’s name, print a message to them. The text of each message should be the same, but each message should be personalized with the person’s name.
+let names = [`Erum`, `Aliza`, `Farwa`, `Unzila`];
+// Loop through each name in the array
+for (let name of names) {
+    // print a personalize message for each friend
+    console.log(`Hello ${name},You are the Semicolon to my Statement`);
+}
---
+++ Q12.ts
@@ -0,0 +1,13 @@
+
+// 12.Greetings: Start with the array you used in Exercise 11, but instead of just printing each person’s name, print a message to them. The text of each message should be the same, but each message should be personalized with the person’s name.
+
+let names: string[]=[`Erum`, `Aliza`, `Farwa`, `Unzila`];
+
+// Loop through each name in the array
+
+for (let name of names)
+{
+
+    // print a personalize message for each friend
+    console.log(`Hello ${name},You are the Semicolon to my Statement`)
+}
